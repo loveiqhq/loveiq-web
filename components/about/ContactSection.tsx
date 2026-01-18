e60419812c71b494cd110749c3ecd3461bc84f5f
Eman Cickusic <cickusiceman@gmail.com>
2026-01-18 18:36:53 +0100

    fix again

--- components/about/ContactSection.tsx
+++ components/about/ContactSection.tsx
@@ -166,8 +166,8 @@ const ContactSection: FC = () => {
     <section id="contact" className="overflow-hidden bg-[#0A0510] px-4 py-16 md:px-6 md:py-24">
       <div className="content-shell mx-auto grid max-w-[1100px] gap-6 lg:grid-cols-2">
         <div className="reveal-on-scroll relative isolate overflow-hidden rounded-[24px] border border-white/10 bg-[#120B1C] p-8 md:rounded-[32px] md:p-16">
-          <div className="pointer-events-none absolute -left-32 -top-32 h-[250px] w-[250px] rounded-full bg-[#2e0147] blur-[100px] md:-left-64 md:-top-64 md:h-[500px] md:w-[500px] md:blur-[200px]" />
-          <div className="pointer-events-none absolute -bottom-24 -right-20 h-[150px] w-[150px] rounded-full bg-[#fe6839] blur-[100px] md:-bottom-52 md:-right-40 md:h-[300px] md:w-[300px] md:blur-[200px]" />
+          <div className="pointer-events-none absolute -left-16 -top-16 h-[200px] w-[200px] rounded-full bg-[#2e0147] blur-[80px] md:-left-64 md:-top-64 md:h-[500px] md:w-[500px] md:blur-[200px]" />
+          <div className="pointer-events-none absolute -bottom-12 -right-10 h-[120px] w-[120px] rounded-full bg-[#fe6839] blur-[80px] md:-bottom-52 md:-right-40 md:h-[300px] md:w-[300px] md:blur-[200px]" />
           <div className="relative">
             <h2 className="font-serif text-4xl font-semibold leading-[1] tracking-[-0.025em] text-white md:text-[60px] md:leading-[60px]">
               Contact Our
